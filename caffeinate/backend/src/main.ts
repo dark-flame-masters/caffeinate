af1839085a5f7f7f4d99bd9c9a994c19909bb386
Jan Garong <42362229+jangarong@users.noreply.github.com>
2022-03-15 23:02:28 -0400

    Add environment variables and docker integration.

--- caffeinate/backend/src/main.ts
+++ caffeinate/backend/src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //session here
-  // somewhere in your initialization file
   app.use(
     session({
-      secret: 'my-secret',
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
     }),
   );
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api', { exclude: [{ path: 'graphql', method: RequestMethod.POST }] })
   await app.listen(process.env.PORT || 3000);
-
 }
 bootstrap();

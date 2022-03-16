011f644a10a4a2bfd5ee5e8563870d6a09f51606
Jan Garong <42362229+jangarong@users.noreply.github.com>
2022-03-15 23:04:37 -0400

    [devops/docker-init2] (#3)
    * Made the port dynamic.
    
    * Add static frontend serving + backend in one app.
    
    * Fix directories and add dockerignore.
    
    * Remove dead code.
    
    * Add api prefix to backend.
    
    * Add environment variables and docker integration.

--- caffeinate/backend/src/app.module.ts
+++ caffeinate/backend/src/app.module.ts
@@ -5,20 +5,29 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({isGlobal: true}),
     MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: 'mongodb://localhost:27017/nest',
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
       }),
+      inject: [ConfigService]
     }),
-
+    ServeStaticModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ([{
+        rootPath: join(__dirname, configService.get('FRONTEND_STATIC_RELDIR')),
+      }]),
+      inject: [ConfigService]
+    }),
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(__dirname, './src/schema.gql'),
       sortSchema: true,
       driver: ApolloDriver,
     }),
@@ -28,4 +37,4 @@ import { AuthModule } from './auth/auth.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- caffeinate/backend/src/main.ts
+++ caffeinate/backend/src/main.ts
@@ -1,21 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
-import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //session here
-  // somewhere in your initialization file
   app.use(
     session({
-      secret: 'my-secret',
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
     }),
   );
-  await app.listen(3000);
-
+  app.setGlobalPrefix('api', { exclude: [{ path: 'graphql', method: RequestMethod.POST }] })
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

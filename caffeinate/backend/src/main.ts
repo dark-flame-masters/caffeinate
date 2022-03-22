1f20f5e57f1f91cf6cd5e3112d0e11b8e28ebc5e
Jan Garong <42362229+jangarong@users.noreply.github.com>
2022-03-22 12:03:01 -0400

    Config https to nest.js server.

--- caffeinate/backend/src/main.ts
+++ caffeinate/backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import * as fs from 'fs'
 import { RequestMethod } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const httpsKey = fs.readFileSync(process.env.HTTPS_PRIVATE_KEY_RELDIR);
+  const httpsCert = fs.readFileSync(process.env.HTTPS_CERTIFICATE_RELDIR);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: httpsKey, cert: httpsCert
+    }
+  });
+
   //session here
   // somewhere in your initialization file
   app.use(
@@ -19,6 +26,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix('api', { exclude: [{ path: 'graphql', method: RequestMethod.POST }] })
-  await app.listen(process.env.PORT || 3000);
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap();

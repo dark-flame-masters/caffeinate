8e240019e991303227c6c487cb29baad59ae5ed0
Jan Garong <42362229+jangarong@users.noreply.github.com>
2022-03-22 19:59:50 -0400

    Add https to config and docker compose.

--- caffeinate/backend/src/main.ts
+++ caffeinate/backend/src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
-import * as fs from 'fs'
 import { RequestMethod } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const httpsKey = fs.readFileSync(process.env.HTTPS_PRIVATE_KEY_RELDIR);
-  const httpsCert = fs.readFileSync(process.env.HTTPS_CERTIFICATE_RELDIR);
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: httpsKey, cert: httpsCert
-    }
-  });
+  const app = await NestFactory.create(AppModule);
 
   //session here
   // somewhere in your initialization file

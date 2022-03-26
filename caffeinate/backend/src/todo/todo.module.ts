78ad689a70d32ec17756b1317c362f22b0b226aa
Meixuan Lu <meixvan.lu@mail.utoronto.ca>
2022-03-25 20:42:24 -0400

    added todo list structure

---
+++ caffeinate/backend/src/todo/todo.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { TodoResolver } from './todo.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Todo, TodoSchema } from './todo.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Todo.name, schema: TodoSchema },
+    ]),
+  ],
+  providers: [TodoService, TodoResolver],
+  exports: [TodoService]
+})
+export class TodoModule {}

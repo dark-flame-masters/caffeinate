3fc6380c9e0984cba57d2a2f8a637ce212861e62
Meixuan Lu <meixvan.lu@mail.utoronto.ca>
2022-03-29 22:02:29 -0400

    updated survey

--- caffeinate/backend/src/survey/survey.schema.ts
+++ caffeinate/backend/src/survey/survey.schema.ts
@@ -1,8 +1,7 @@
-import { Field, ObjectType, ID, InputType, Int } from '@nestjs/graphql';
+import { Field, ObjectType, InputType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { IsNotEmpty, Matches } from 'class-validator';
 import * as mongoose from 'mongoose';
-import { User } from 'src/users/users.schema';
 
 export type SurveyDocument = Survey & mongoose.Document;
 
@@ -54,15 +53,4 @@ export class CreateSurveyInput {
     @IsNotEmpty()
     @Matches(/([A-Za-z0-9\s\-':()!.,;?])+/)
     answer2: string;
-}
-
-@InputType()
-export class FindSurveyInput {
-
-  @Field()
-  @Matches(/([A-Za-z0-9\s\-':()!.,;?])+/)
-  authorGoogleId: string;
-
-  @Field()
-  index: number;
 }
